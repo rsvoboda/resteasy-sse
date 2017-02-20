c421493f52f111eedfdc527a206225dc16c825d6
Rostislav Svoboda <rsvoboda@redhat.com>
2017-02-20 15:28:27 +0100

    Fix of SseResource initialization as getSingletons() is called twice https://github.com/resteasy/Resteasy/blob/master/resteasy-jaxrs/src/main/java/org/jboss/resteasy/spi/ResteasyDeployment.java#L539..L541

--- src/main/java/com/github/rsvoboda/sse/JAXRSConfiguration.java
+++ src/main/java/com/github/rsvoboda/sse/JAXRSConfiguration.java
@@ -1,6 +1,5 @@
 package com.github.rsvoboda.sse;
 
-import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 import javax.ws.rs.ext.Provider;
 import java.util.HashSet;
@@ -8,17 +7,18 @@ import java.util.Set;
 
 /**
  * Configures a JAX-RS endpoint.
- *
  */
 //@ApplicationPath("service")
 @Provider
 public class JAXRSConfiguration extends Application {
-    private Set<Object> singletons = new HashSet<Object>();
+    private Set<Object> singletons = new HashSet<>();
+
+    public JAXRSConfiguration() {
+        singletons.add(new SseResource());
+    }
 
     @Override
-    public Set<Object> getSingletons()
-    {
-        singletons.add(new SseResource());
+    public Set<Object> getSingletons() {
         return singletons;
     }
 }
